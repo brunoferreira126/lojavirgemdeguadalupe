23da13e46b1e31ca1dfab70918f928a487a91f25
brunoferreira126 <ferreira_brunomendes@hotmail.com>
2024-11-08 02:27:11 -0300

    Melhora do layout do cartão de visita interativo

---
+++ script.js
@@ -0,0 +1,12 @@
+// Função para redirecionar o usuário ao clicar nos botões
+document.getElementById('whatsapp').addEventListener('click', function() {
+    window.open('https://wa.me/message/QZZMFVE4Q5NSC1', '_blank');
+});
+
+document.getElementById('facebook').addEventListener('click', function() {
+    window.open('https://www.facebook.com/share/Qpi6W5gFYGMEenXR/?mibextid=LQQJ4d', '_blank');
+});
+
+document.getElementById('instagram').addEventListener('click', function() {
+    window.open('https://www.instagram.com/lojavirgemdeguadalupe', '_blank');
+});
